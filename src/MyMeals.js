5a810b526f070878794c4bde274327d5ced5734c
Maria Sokolova <92234362+mamizheva85@users.noreply.github.com>
2023-05-04 16:01:57 -0400

    Add files via upload

---
+++ src/MyMeals.js
@@ -0,0 +1,12 @@
+import {AiFillEdit, AiFillDelete} from "react-icons/ai"
+
+export const MyMeals = ({ text, updatingInInput, deleteMeal}) => {
+	console.log(text)
+	return (
+		<div>
+			<p>{text}</p>
+			<AiFillEdit onClick={updatingInInput}></AiFillEdit>
+			<AiFillDelete onClick={deleteMeal}></AiFillDelete>
+		</div>
+	)
+} 
